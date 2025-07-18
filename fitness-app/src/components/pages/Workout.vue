d284166959dbbb627790756de475b39f661398d8
senpaiharde <xxslavan1@gmail.com>
2025-07-18 20:28:06 +0300

    hitting up the display of the workout option

--- fitness-app/src/components/pages/Workout.vue
+++ fitness-app/src/components/pages/Workout.vue
@@ -1,10 +1,18 @@
-<script setup></script>
+<script setup>
+import { workoutProgram } from '../../utils';
+const selectedWorkout = 4; // Example selected workout
+const { workout, warmup } = workoutProgram[selectedWorkout];
+</script>
 
 <template>
-    <div class="welcome-container">
-        <h1 class="welcome-title">Welcome to the Fitness App!</h1>
-        <p class="welcome-message">Get ready to track your fitness journey and achieve your goals!</p>
+  <section id="workout-card">
+    <div class="plan-card card">
+      <div class="plan-card-header">
+        <p>Day {{ selectedWorkout < 9 ? '0' + selectedWorkout : selectedWorkout }}</p>
+      </div>
     </div>
+    <button @click="() => handleChangeDisplay(2)">Back to Dashboard &larr;</button>
+  </section>
 </template>
 
 <style scoped></style>
