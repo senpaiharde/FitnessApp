de5be78f85a696b2be82f45be7990a9d03cf8d87
senpaiharde <xxslavan1@gmail.com>
2025-07-18 20:10:47 +0300

    renedering the grid days

--- fitness-app/src/components/Grid.vue
+++ fitness-app/src/components/Grid.vue
@@ -4,13 +4,19 @@ const workoutTypes = ['push', 'pull', 'legs'];
 </script>
 
 <template>
-<section id="grid">
-  <button class="card-button plan-card">
-    <div class="card">
-     <p class="card-title">Workout Plan</p>
-        </div>
-  </button>
-</section>
+  <section id="grid">
+    <button
+     :key="workoutIdx"
+      v-for="(workout, workoutIdx) in Object.keys(workoutProgram)"
+      class="card-button plan-card"
+    >
+      <div class="card">
+        <p class="card-title">{{ workout < 9 ? '0' + (workoutIdx + 1) : workoutIdx + 1 }}</p>
+        ICON
+      </div>
+      <h3>Types</h3>
+    </button>
+  </section>
 </template>
 
 <style scoped></style>
