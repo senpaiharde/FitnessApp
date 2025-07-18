da0edba069d3dc2dd8d7329efce5bd0e7c9a97ea
senpaiharde <xxslavan1@gmail.com>
2025-07-18 20:51:32 +0300

    showing full section of warmup + workout

--- fitness-app/src/components/pages/Workout.vue
+++ fitness-app/src/components/pages/Workout.vue
@@ -9,8 +9,39 @@ const { workout, warmup } = workoutProgram[selectedWorkout];
     <div class="plan-card card">
       <div class="plan-card-header">
         <p>Day {{ selectedWorkout < 9 ? '0' + selectedWorkout : selectedWorkout }}</p>
+        <i class="fa-solid fa-dumbbell"></i>
+      </div>
+      <h2>{{}}</h2>
+    </div>
+    <div class="workout-grid">
+      <h4 class="grid-name">Warmup</h4>
+      <h6 class="grid-name">sets</h6>
+      <h6 class="grid-name">reps</h6>
+      <h6 class="grid-weights">weights</h6>
+      <div v-for="(exercise, idx) in warmup" :key="idx" class="workout-grid-row">
+        <div class="grid-name">
+          <p>{{ exercise.name }}</p>
+          <button>
+            <i class="fa-regular fa-circle-question"></i>
+          </button>
+        </div>
+        <p>{{ exercise.sets }}</p>
+        <p>{{ exercise.reps }}</p>
+        <input class="grid-weights" type="text" placeholder="23Kg" disabled />
+      </div>
+      <div v-for="(exercise, idx) in workout" :key="idx" class="workout-grid-row">
+        <div class="grid-name">
+          <p>{{ exercise.name }}</p>
+          <button>
+            <i class="fa-regular fa-circle-question"></i>
+          </button>
+          <p>{{ exercise.sets }}</p>
+          <p>{{ exercise.reps }}</p>
+          <input class="grid-weights" type="text" placeholder="23Kg"  />
+        </div>
       </div>
     </div>
+
     <button @click="() => handleChangeDisplay(2)">Back to Dashboard &larr;</button>
   </section>
 </template>
