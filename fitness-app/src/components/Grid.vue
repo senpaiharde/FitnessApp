a4d890290c4b848e3a2b90f9342eec049864d5d4
senpaiharde <xxslavan1@gmail.com>
2025-07-18 20:14:46 +0300

    added cute icons on each day

--- fitness-app/src/components/Grid.vue
+++ fitness-app/src/components/Grid.vue
@@ -6,13 +6,15 @@ const workoutTypes = ['push', 'pull', 'legs'];
 <template>
   <section id="grid">
     <button
-     :key="workoutIdx"
+      :key="workoutIdx"
       v-for="(workout, workoutIdx) in Object.keys(workoutProgram)"
       class="card-button plan-card"
     >
       <div class="card">
         <p class="card-title">{{ workout < 9 ? '0' + (workoutIdx + 1) : workoutIdx + 1 }}</p>
-        ICON
+        <i class="card-icon fa-solid fa-dumbbell" v-if="workout % 3 == 0"></i>
+        <i class="card-icon fa-solid fa-weight-hanging" v-if="workout % 3 == 1"></i>
+        <i class="card-icon fa-solid fa-bolt" v-if="workout % 3 == 2"></i>
       </div>
       <h3>Types</h3>
     </button>
