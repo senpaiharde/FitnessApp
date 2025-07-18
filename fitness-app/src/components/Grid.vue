84c07890c2d342bd3452a7ae130fe208e92004f4
senpaiharde <xxslavan1@gmail.com>
2025-07-18 19:52:27 +0300

    working on grid he will hold the cards that getting displayed

--- fitness-app/src/components/Grid.vue
+++ fitness-app/src/components/Grid.vue
@@ -1,5 +1,16 @@
-<script setup></script>
+<script setup>
+import { workoutProgram } from '../utils';
+const workoutTypes = ['push', 'pull', 'legs'];
+</script>
 
-<template>23</template>
+<template>
+<section id="grid">
+  <button class="card-button plan-card">
+    <div class="card">
+     <p class="card-title">Workout Plan</p>
+        </div>
+  </button>
+</section>
+</template>
 
 <style scoped></style>
--- fitness-app/src/components/pages/Dashboard.vue
+++ fitness-app/src/components/pages/Dashboard.vue
@@ -1,21 +1,48 @@
 <script setup>
-import {gymHealthFacts} from '../../utils/index';
+import { gymHealthFacts } from '../../utils/index';
+import Grid from '../Grid.vue';
 const randomFact = gymHealthFacts[Math.floor(Math.random() * gymHealthFacts.length)];
 </script>
 
 <template>
-    <section id="dashboard">
-        <div class="card tip-container">
-            <h2 >Welcome Natty</h2>
-            <div>
-              
-                <p class="tip"><strong> Daily Tip</strong>
-                <br/> Inject the Tip
-            </p>
-            </div>
-        </div>
-            </section>
-      
+  <section id="dashboard">
+    <div class="card tip-container">
+      <h2>Welcome Natty</h2>
+      <div>
+        <p class="tip">
+          <strong> Daily Tip</strong> <br />
+          {{ randomFact }}
+        </p>
+      </div>
+      <button @click="() => handleChangeDisplay(3)">Start Workout &rarr;</button>
+    </div>
+    <Grid />
+  </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+.tip-container, .tip-container div,
+#dashboard {
+  display: flex;
+  
+}
+.tip-container,
+#dashboard {
+  flex-direction: column;
+ 
+}
+#dashboard {
+  gap: 2rem;
+}
+.tip-container {
+  gap: 0.5rem;
+  
+}
+@media (min-width: 640px) {
+  
+  .tip-container {
+    gap: 1rem;
+  }
+    
+}
+</style>
