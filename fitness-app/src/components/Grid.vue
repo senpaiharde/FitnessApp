0e79e734b0db6912f82da0085e38a8a9b03d0b0a
senpaiharde <xxslavan1@gmail.com>
2025-07-18 20:21:41 +0300

    added styles

--- fitness-app/src/components/Grid.vue
+++ fitness-app/src/components/Grid.vue
@@ -16,9 +16,36 @@ const workoutTypes = ['push', 'pull', 'legs'];
         <i class="card-icon fa-solid fa-weight-hanging" v-if="workout % 3 == 1"></i>
         <i class="card-icon fa-solid fa-bolt" v-if="workout % 3 == 2"></i>
       </div>
-      <h3>Types</h3>
+      <h3>{{ workoutTypes[workoutIdx % 3] }}</h3>
     </button>
   </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+#grid {
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  gap: 1rem;
+}
+#grid button {
+  width: 100%;
+}
+.plan-card {
+  display: flex;
+  flex-direction: column;
+}
+.plan-card div {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 0.5rem;
+}
+.plan-card dev p {
+  text-align: left;
+}
+@media (min-width: 640px) {
+  #grid {
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+  }
+}
+</style>
