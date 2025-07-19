8f5b04d933654345c29e419f2e1e8617b6a4a98f
senpaiharde <xxslavan1@gmail.com>
2025-07-19 19:54:38 +0300

    added display of vaild information basic on what selected

--- fitness-app/src/components/pages/Workout.vue
+++ fitness-app/src/components/pages/Workout.vue
@@ -1,12 +1,30 @@
 <script setup>
-import { workoutProgram } from '../../utils';
+import { workoutProgram, exerciseDescriptions } from '../../utils';
 import Portal from '../Portal.vue';
 const selectedWorkout = 4; // Example selected workout
 const { workout, warmup } = workoutProgram[selectedWorkout];
+const selectedExercise = workout[3]; // Example selected exercise
+const exerciseDescription =
+  exerciseDescriptions[selectedExercise.name] || 'No description available';
 </script>
 
 <template>
-  <Portal> <h4> PUSHUPS   </h4> </Portal>
+  <Portal>
+    <div class="exercise-description">
+      <h4>{{ selectedExercise.name }}</h4>
+
+      <div class="exercise-image">
+        <small>Description</small>
+        <p class="exercise-description">
+          {{ exerciseDescription || 'No description available for this exercise.' }}
+        </p>
+      </div>
+      <button @click="$emit('close')">
+        Close
+        <i class="fa-solid fa-xmark"></i>
+      </button>
+    </div>
+  </Portal>
   <section id="workout-card">
     <div class="plan-card card">
       <div class="plan-card-header">
@@ -131,5 +149,10 @@ const { workout, warmup } = workoutProgram[selectedWorkout];
 }
 .workout-btns button i {
   padding-left: 0.5rem;
+}
+.exercise-description {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 }
 </style>
