19d6dccef6dfa3c573ba97325456662c5ff5a409
senpaiharde <xxslavan1@gmail.com>
2025-08-05 21:44:38 +0300

    moving logic of steps,timer , to other function

--- fitness-app/src/components/pages/Workout.vue
+++ fitness-app/src/components/pages/Workout.vue
@@ -5,6 +5,7 @@ import Portal from '../Portal.vue';
 import draggable from 'vuedraggable';
 import draggableComponent from 'vuedraggable';
 import Warmup from '../sideComponents/Warmup.vue';
+import Timer from '../sideComponents/Timer.vue';
 const props = defineProps({
   handleReorderWorkout: Function,
   handleDeleteExercise: Function,
@@ -18,44 +19,22 @@ const props = defineProps({
   isWorkoutComplete: Boolean,
   handleSaveWorkout: Function,
 });
-const workoutTypes = ['push', 'pull', 'legs'];
+
 const workout = computed({
   get: () => props.workoutData?.[props.selectedWorkout] || [],
   set: (newValue) => {
     props.workoutData[props.selectedWorkout] = newValue;
   },
 });
-const warmup = computed(() => workoutProgram[props.selectedWorkout]?.warmup || []);
+
 let selectedExercise = ref(null);
 
 const exerciseDescription = computed(() => exerciseDescriptions[selectedExercise.value]);
-console.log('Selected workout:', props.selectedWorkout, 'Workout data:', props.workoutData);
-console.log(workout.value, 'this is workout');
-const day = props.selectedWorkout + 1;
-
-let timers = reactive({ ...props.timerData } || {});
-let time = ref(0);
-let minute = ref(0);
-let hour = ref(0);
-let timerId = ref(null);
-let localSteps = ref(props.steps || 0);
-console.log(
-  'Workout component loaded with props:',
-  props.workoutData[props.selectedWorkout],
-  'Selected workout:',
-  props.selectedWorkout
-);
 
 const reactiveData = toRef(props, 'workoutData');
 const data = computed(() => reactiveData.value);
 console.log(data.value, 'data', props, 'while props');
 onMounted(() => {
-  const saved = timers[props.selectedWorkout] || { time: 0, minute: 0, hour: 0 };
-  time.value = saved.time || 0;
-  minute.value = saved.minute || 0;
-  hour.value = saved.hour || 0;
-
-  localSteps.value = props.steps || 0;
   console.log('Mounted workout, preloaded values:', data.value[props.selectedWorkout]);
 
   for (const key in workout.value) {
@@ -66,88 +45,6 @@ onMounted(() => {
     workout.value[key].weight;
   }
 });
-watch(
-  () => props.selectedWorkout,
-  (idx) => {
-    const saved = timers[idx] || { time: 0, minute: 0, hour: 0 };
-    time.value = saved.time;
-    minute.value = saved.minute;
-    hour.value = saved.hour;
-  },
-
-  { immediate: true }
-);
-
-function clearTimer() {
-  if (timerId.value !== null) {
-    clearInterval(timerId.value);
-    timerId.value = null;
-    time.value = 0;
-    minute.value = 0;
-    hour.value = 0;
-  }
-}
-let click = 0;
-// 2) start/stop functions
-function startTimer() {
-  click++;
-  if (click % 2 === 0) {
-    stopTimer();
-    click = 0;
-    return;
-  }
-  // if you already have one running, clear it
-  if (timerId.value !== null) {
-    clearInterval(timerId.value);
-  }
-
-  time.value = time.value || 0; // reset time if needed
-  timerId.value = setInterval(() => {
-    time.value++;
-
-    if (time.value % 60 === 0) {
-      minute.value++; // increment minute every 60 seconds
-      time.value = 0;
-    }
-    if (minute.value === 60 && hour.value < 24) {
-      hour.value++; // increment hour every 60 minutes
-      minute.value = 0; // reset minute
-    }
-  }, 1000);
-}
-
-function stopTimer() {
-  if (timerId.value !== null) {
-    clearInterval(timerId.value);
-    timerId.value = null;
-    console.log('Timer stopped');
-  }
-  timers[props.selectedWorkout] = {
-    time: time.value,
-    minute: minute.value,
-    hour: hour.value,
-  };
-  props.handleSaveTimerData(timers);
-
-  //localStorage.setItem('timerData', JSON.stringify(timers));
-}
-const stepsPortal = ref(null);
-const handleSkip = () => {
-  workout.value.forEach((exercise) => {
-    exercise.sets = '';
-    exercise.reps = '';
-    exercise.weight = '';
-  });
-  props.handleSaveWorkout(workout.value);
-};
-
-const saveSteps = () => {
-  if (localSteps.value < 1) {
-    console.warn('Please enter a valid number of steps.');
-    return;
-  }
-  props.handleSaveSteps(localSteps.value);
-};
 
 const values = reactive({
   name: '',
@@ -176,6 +73,15 @@ function handleSaveNewWorkout(value) {
   values.weight = '';
 }
 console.log(workout.value, 'workout');
+const handleSkip = () => {
+    console.log('fireing')
+  workout.value.forEach((exercise) => {
+    exercise.sets = '';
+    exercise.reps = '';
+    exercise.weight = '';
+  });
+  props.handleSaveWorkout(workout.value);
+};
 </script>
 
 <template>
@@ -196,79 +102,22 @@ console.log(workout.value, 'workout');
         </button>
       </div>
     </Portal>
-    <Portal :onClose="() => (stepsPortal = null)" v-if="stepsPortal">
-      <div class="exercise-description">
-        <h3>{{ selectedExercise }}</h3>
 
-        <div class="exercise-image">
-          <small>Description </small>
-          <p class="exercise-description">
-            {{ exerciseDescription || 'No description available for this exercise.' }}
-          </p>
-        </div>
-        <div class="exercise-image">
-          <small>Steps</small>
-          <p class="exercise-description">
-            {{ localSteps || 'No steps recorded for this workout.' }}
-          </p>
-          <p class="exercise-description">
-            while you Skipped the workout, you can add your steps here.
-            <br />
-            <input v-model="localSteps" type="range" :range="(0, 20000)" min="0" max="20000" />
-          </p>
-
-          <div class="exercise-btns">
-            <button
-              @click="() => ((stepsPortal = null), saveSteps, console.log(saveSteps, 'steps'))"
-            >
-              Close
-              <i class="fa-solid fa-xmark"></i>
-            </button>
-            <button @click="handleSaveWorkout">
-              Next workout
-              <i class="fa-solid fa-arrow-right"></i>
-            </button>
-          </div>
-        </div>
-      </div>
-    </Portal>
     <section id="workout-card">
-      <div class="plan-card card">
-        <div class="plan-card-header">
-          <p>Day {{ day <= 9 ? '0' + day : day }}</p>
-          <div class="plan-card-header">
-            <button @click="startTimer">
-              <i class="fa-solid fa-play"></i>
-            </button>
-            <button @click="stopTimer">
-              <i class="fa-solid fa-pause"></i>
-            </button>
-            <button @click="clearTimer">
-              <i class="fa-solid fa-stop"></i>
-            </button>
-
-            <button disabled class="time">
-              {{ hour < 10 ? '0' + hour : hour }}: {{ minute < 10 ? '0' + minute : minute }}:
-              {{ time < 10 ? '0' + time : time }}
-            </button>
-          </div>
-          <i class="fa-solid fa-dumbbell"></i>
-        </div>
-
-        <h2 class="workoutName">{{ workoutTypes[props.selectedWorkout % 3] }} Workout</h2>
-
-        <button
-          v-if="workoutTypes[props.selectedWorkout % 3] === 'legs'"
-          @click="
-            () => {
-              stepsPortal = true;
-              handleSkip();
-            }
-          "
-        >
-          {{ workoutTypes[props.selectedWorkout % 3] === 'legs' ? 'Steps Time?' : '' }}
-        </button>
-      </div>
+      <Timer
+         :handleSkip="handleSkip"
+        :handleReorderWorkout="props.handleReorderWorkout"
+        :handleDeleteExercise="props.handleDeleteExercise"
+        :handleAddWorkout="props.handleAddWorkout"
+        :timerData="props.timerData"
+        :steps="props.steps"
+        :workoutData="props.workoutData"
+        :handleSaveSteps="props.handleSaveSteps"
+        :handleSaveTimerData="props.handleSaveTimerData"
+        :isWorkoutComplete="props.isWorkoutComplete"
+        :handleSaveWorkout="props.handleSaveWorkout"
+        :selectedWorkout="props.selectedWorkout"
+      />
       <div class="workout-grid">
         <h4 class="grid-name">Warmup</h4>
         <h6>sets</h6>
@@ -423,7 +272,7 @@ console.log(workout.value, 'workout');
 #workout-card {
   gap: 1.5rem;
 }
-.plan-card-header,
+
 .workout-btns {
   display: flex;
   align-items: center;
@@ -485,16 +334,5 @@ console.log(workout.value, 'workout');
 .workout-btns button i {
   padding-left: 0.5rem;
 }
-.exercise-description {
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  width: 100%;
-}
-.exercise-description h3 {
-  text-transform: capitalize;
-}
-.exercise-description button i {
-  padding-left: 0.5rem;
-}
+
 </style>
---
+++ fitness-app/src/components/sideComponents/Timer.vue
@@ -0,0 +1,222 @@
+<script setup>
+import Portal from '../Portal.vue';
+import { computed, onMounted, ref, watch, reactive, toRef } from 'vue';
+const props = defineProps({
+  handleSkip: Function,
+  handleReorderWorkout: Function,
+  handleDeleteExercise: Function,
+  handleAddWorkout: Function,
+  handleSaveSteps: Function,
+  handleSaveTimerData: Function,
+  timerData: Object,
+  steps: Number,
+  workoutData: Object,
+  selectedWorkout: Number,
+  isWorkoutComplete: Boolean,
+  handleSaveWorkout: Function,
+});
+console.log(props, 'data');
+const workoutTypes = ['push', 'pull', 'legs'];
+const day = props.selectedWorkout + 1;
+let timers = reactive({ ...props.timerData } || {});
+let time = ref(0);
+let minute = ref(0);
+let hour = ref(0);
+let timerId = ref(null);
+let localSteps = ref(props.steps || 0);
+console.log(
+  'Workout component loaded with props:',
+  props.workoutData[props.selectedWorkout],
+  'Selected workout:',
+  props.selectedWorkout
+);
+
+const reactiveData = toRef(props, 'workoutData');
+const data = computed(() => reactiveData.value);
+console.log(data.value, 'data', props, 'while props');
+onMounted(() => {
+  const saved = timers[props.selectedWorkout] || { time: 0, minute: 0, hour: 0 };
+  time.value = saved.time || 0;
+  minute.value = saved.minute || 0;
+  hour.value = saved.hour || 0;
+
+  localSteps.value = props.steps || 0;
+});
+watch(
+  () => props.selectedWorkout,
+  (idx) => {
+    const saved = timers[idx] || { time: 0, minute: 0, hour: 0 };
+    time.value = saved.time;
+    minute.value = saved.minute;
+    hour.value = saved.hour;
+  },
+
+  { immediate: true }
+);
+
+function clearTimer() {
+  if (timerId.value !== null) {
+    clearInterval(timerId.value);
+    timerId.value = null;
+    time.value = 0;
+    minute.value = 0;
+    hour.value = 0;
+  }
+}
+let click = 0;
+// 2) start/stop functions
+function startTimer() {
+  click++;
+  if (click % 2 === 0) {
+    stopTimer();
+    click = 0;
+    return;
+  }
+  // if you already have one running, clear it
+  if (timerId.value !== null) {
+    clearInterval(timerId.value);
+  }
+
+  time.value = time.value || 0; // reset time if needed
+  timerId.value = setInterval(() => {
+    time.value++;
+
+    if (time.value % 60 === 0) {
+      minute.value++; // increment minute every 60 seconds
+      time.value = 0;
+    }
+    if (minute.value === 60 && hour.value < 24) {
+      hour.value++; // increment hour every 60 minutes
+      minute.value = 0; // reset minute
+    }
+  }, 1000);
+}
+
+function stopTimer() {
+  if (timerId.value !== null) {
+    clearInterval(timerId.value);
+    timerId.value = null;
+    console.log('Timer stopped');
+  }
+  timers[props.selectedWorkout] = {
+    time: time.value,
+    minute: minute.value,
+    hour: hour.value,
+  };
+  props.handleSaveTimerData(timers);
+
+  //localStorage.setItem('timerData', JSON.stringify(timers));
+}
+const stepsPortal = ref(null);
+
+
+const saveSteps = () => {
+  if (localSteps.value < 1) {
+    console.warn('Please enter a valid number of steps.');
+    return;
+  }
+  props.handleSaveSteps(localSteps.value);
+};
+</script>
+
+<template>
+  <Portal :onClose="() => (stepsPortal = null)" v-if="stepsPortal">
+    <div class="exercise-description">
+      <h3>{{ selectedExercise }}</h3>
+
+      <div class="exercise-image">
+        <small>Description </small>
+        <p class="exercise-description">
+          {{ exerciseDescription || 'No description available for this exercise.' }}
+        </p>
+      </div>
+      <div class="exercise-image">
+        <small>Steps</small>
+        <p class="exercise-description">
+          {{ localSteps || 'No steps recorded for this workout.' }}
+        </p>
+        <p class="exercise-description">
+          while you Skipped the workout, you can add your steps here.
+          <br />
+          <input v-model="localSteps" type="range" :range="(0, 20000)" min="0" max="20000" />
+        </p>
+
+        <div class="exercise-btns">
+          <button @click="() => ((stepsPortal = null), saveSteps, console.log(saveSteps, 'steps'))">
+            Close
+            <i class="fa-solid fa-xmark"></i>
+          </button>
+          <button @click="props.handleSkip">
+            Next workout
+            <i class="fa-solid fa-arrow-right"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  </Portal>
+  <div class="plan-card card">
+    <div class="plan-card-header">
+      <p>Day {{ day <= 9 ? '0' + day : day }}</p>
+      <div class="plan-card-header">
+        <button @click="startTimer">
+          <i class="fa-solid fa-play"></i>
+        </button>
+        <button @click="stopTimer">
+          <i class="fa-solid fa-pause"></i>
+        </button>
+        <button @click="clearTimer">
+          <i class="fa-solid fa-stop"></i>
+        </button>
+
+        <button disabled class="time">
+          {{ hour < 10 ? '0' + hour : hour }}: {{ minute < 10 ? '0' + minute : minute }}:
+          {{ time < 10 ? '0' + time : time }}
+        </button>
+      </div>
+      <i class="fa-solid fa-dumbbell"></i>
+    </div>
+
+    <h2 class="workoutName">{{ workoutTypes[props.selectedWorkout % 3] }} Workout</h2>
+
+    <button
+      v-if="workoutTypes[props.selectedWorkout % 3] === 'legs'"
+      @click="
+        () => {
+          stepsPortal = true;
+         
+        }
+      "
+    >
+      {{ workoutTypes[props.selectedWorkout % 3] === 'legs' ? 'Steps Time?' : '' }}
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.exercise-btns {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 0.5rem;
+}
+.plan-card-header,
+.workout-btns {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+}
+.exercise-description {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+}
+.exercise-description h3 {
+  text-transform: capitalize;
+}
+.exercise-description button i {
+  padding-left: 0.5rem;
+}
+</style>
